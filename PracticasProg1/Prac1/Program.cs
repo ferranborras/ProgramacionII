42fe75e5ca09aa506f87e3c029d1a8edecb76115
FBorras <ferranbb2003@gmail.com>
2022-02-15 15:58:25 +0100

    Prac1

---
+++ PracticasProg1/Prac1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Prac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la cantidad que quieres comprar.");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            if (numero < 200)
+            {
+                Console.WriteLine("El precio es de " + numero * 4.50f + " eur.");
+                Console.WriteLine("4.50 eur la unidad.");
+            } else if (numero >=200 && numero <= 500)
+            {
+                Console.WriteLine("El precio es de " + numero * 3.75f + " eur.");
+                Console.WriteLine("3.75 eur la unidad.");
+            } else
+            {
+                Console.WriteLine("El precio es de " + numero * 3.25f + " eur.");
+                Console.WriteLine("3.25 eur la unidad.");
+            }
+            string salir = Console.ReadLine();
+        }
+    }
+}
