50d42305496e70cf59989158d11947b285465217
FBorras <ferranbb2003@gmail.com>
2022-02-15 16:23:08 +0100

    Ej2

---
+++ Ej2/Ej2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe una cadena: ");
+            string cadena = Console.ReadLine();
+            string reversa = "";
+            foreach (char character in cadena)
+                reversa = character + reversa;
+            if (reversa == cadena)
+            {
+                Console.WriteLine("La cadena " + cadena + " es capicúa.");
+            } else
+            {
+                Console.WriteLine("La cadena " + cadena + " no es capicúa.");
+            }
+            string salir = Console.ReadLine();
+        }
+    }
+}
